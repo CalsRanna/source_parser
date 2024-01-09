e73655927bf8881f76db4d45b4820c8a5cb53347
Cals Ranna <cals@Calss-MBP.lan>
2024-01-10 00:22:58 +0800

    feat(message): calcualte snakbar width auto

--- lib/util/message.dart
+++ lib/util/message.dart
@@ -6,17 +6,25 @@ class Message {
   Message.of(this.context);
 
   void show(String message) {
+    if (!context.mounted) return;
     final theme = Theme.of(context);
     final colorScheme = theme.colorScheme;
     final primary = colorScheme.primary;
     final onPrimary = colorScheme.onPrimary;
     final messenger = ScaffoldMessenger.of(context);
     messenger.hideCurrentSnackBar();
+    final textPainter = TextPainter(
+      text: TextSpan(text: message),
+      textDirection: TextDirection.ltr,
+    );
+    textPainter.layout();
+    final width = textPainter.width + 36;
     messenger.showSnackBar(SnackBar(
       backgroundColor: primary,
       behavior: SnackBarBehavior.floating,
       content: Text(message, style: TextStyle(color: onPrimary)),
       duration: const Duration(seconds: 1),
+      width: width,
     ));
   }
 }
